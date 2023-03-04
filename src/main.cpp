e49e511e4c0c9e63a829e788e20ac94f18056ae3
nesmy <pnesmy@gmail.com>
2023-03-04 13:49:33 -0500

    macopen

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,20 @@ int main()
 {
     //glfw: initialize and configure
     //--------------------------------------
-    glfwInit();
+    if(!glfwInit())
+    {
+        std::cout << "init failed" << std::endl;
+        return -1;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
     //glfw window creation
     //-------------------------------------------
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    std::cout <<"window" << std::endl;
     if(window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
