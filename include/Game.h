bc0412212f4fb2e69da18c6dbc0c35fa0a0bed80
nesmy <pnesmy@gmail.com>
2023-03-05 09:55:48 -0500

    camera

---
+++ include/Game.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <Shader.h>
+#include <Camera.h>
+
+#include <iostream>
+
+class Game
+{
+    public:
+    Game();
+    ~Game();
+
+    bool init();
+    void Event();
+    void Update();
+    void Render();
+    void Quit();
+
+    private:
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+    void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+    bool success;
+    const unsigned int SCR_WIDTH = 800;
+    const unsigned int SCR_HEIGHT = 600;
+    GLFWwindow* window = NULL;
+    Shader ourShader;
+    // camera
+    Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+    float lastX = SCR_WIDTH / 2.0f;
+    float lastY = SCR_HEIGHT / 2.0f;
+    bool firstMouse = true;
+
+    // timing
+    float deltaTime = 0.0f;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 b2float lastFrame = 0.0f;
+    bool firstMouse = true;
+    unsigned int VBO, VAO;
+    unsigned int texture1, texture2;
+    float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+    float pitch =  0.0f;
+    float lastX =  800.0f / 2.0;
+    float lastY =  600.0 / 2.0;
+    float fov   =  45.0f;
+
+};
